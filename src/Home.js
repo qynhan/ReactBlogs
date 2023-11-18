32550c6217407955c5b9825da89559b2602eaea2
qynhan <qhan33@uwo.ca>
2023-11-18 17:44:36 -0500

    added jason server for blog data

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([    
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog=>blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [name, setName] = useState('mario');
+
+    // function runs everytime there's a re-render
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data=>{
+                console.log(data);
+                setBlogs(data);
+            })
+    },[]);
+
     return ( 
-        <div classname="home">
-            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
+            <p>{ name }</p>
         </div>
     );
 }
