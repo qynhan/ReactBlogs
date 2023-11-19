b557414758f20a58a9cbc9e478b3d1dc44a99303
qynhan <qhan33@uwo.ca>
2023-11-19 18:34:11 -0500

    Routing to blog details

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 
@@ -7,9 +6,9 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            { error && <div>{ error }</div>}
-            { isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All blogs" />}
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} />}
         </div>
     );
 }
